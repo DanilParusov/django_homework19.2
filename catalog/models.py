6acd51d5fc8a039cadc30496f903abd49df65e9b
Danil <parusovdanik@gmail.com>
2023-07-23 14:15:37 +0300

    add migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='название')
+    description = models.CharField(max_length=200, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='название')
+    description = models.CharField(max_length=200, verbose_name='описание', **NULLABLE)
+    image = models.ImageField(upload_to='product_image/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    creation_date = models.DateField()
+    last_modification_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
