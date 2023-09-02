88d0301a9208ce43280e793c288827a0ea470b52
DanilParusov <parusovdanik@gmail.com>
2023-09-02 20:08:14 +0300

    detail_view_product page cached

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import ProductListView, ProductCreateView, ProductDetailView, ProductDeleteView, ProductUpdateView, \
     CategoryListView
@@ -9,7 +11,7 @@ urlpatterns = [
     # path('contacts/', contacts),
     path('create/', ProductCreateView.as_view(), name='create'),
     path('', ProductListView.as_view(), name='product_list'),
-    path('view/<int:pk>', ProductDetailView.as_view(), name='product_view'),
+    path('view/<int:pk>', cache_page(60)(ProductDetailView.as_view()), name='product_view'),
     path('edit/<int:pk>', ProductUpdateView.as_view(), name='product_edit'),
     path('delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
     path('categories/', CategoryListView.as_view(), name='category_list'),
